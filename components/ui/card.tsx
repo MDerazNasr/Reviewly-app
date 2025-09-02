a3171334d7bd935a9cdbeed8fda37010b7e1c167
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2025-09-01 23:12:19 -0400

    update component syntax

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-const Card = React.forwardRef
+const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
@@ -16,7 +16,7 @@ const Card = React.forwardRef
 ))
 Card.displayName = "Card"
 
-const CardHeader = React.forwardRef
+const CardHeader = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
@@ -24,7 +24,7 @@ const CardHeader = React.forwardRef
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef
+const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
@@ -36,7 +36,7 @@ const CardTitle = React.forwardRef
 ))
 CardTitle.displayName = "CardTitle"
 
-const CardContent = React.forwardRef
+const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
